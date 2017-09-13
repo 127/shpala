b170141a4345c21de20d8f14e9ef6bd7268535c0
127 <mamyashev.marat@gmail.com>
2017-09-13 14:50:23 +0300

    +init

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+
+date_default_timezone_set('Europe/Moscow');
+
+$GLOBALS['APP_ENV'] = (false === getenv('APP_ENV') ? 'development' : getenv('APP_ENV') );
+$GLOBALS['APP_DIR'] = realpath(__DIR__);
+
+$db = require_once "config/db.php";
+$GLOBALS['DB'] = $db[$GLOBALS['APP_ENV']];
+
+require_once 'lib/autoload.php';
+
+$a = new AppController();
+?>
