98bcdc0b02aa126c00e286c1cfe2afda557d981b
127 <mamyashev.marat@gmail.com>
2018-10-16 23:35:33 +0300

    minor dix

--- lib/doorz/base_view.php
+++ lib/doorz/base_view.php
@@ -1,11 +1,28 @@
 <?php
 class BaseView {
-	public function __construct() {
-		
+	protected $path = '/app/views/';
+	protected $extension = '.phtml';
+	protected $action_file; 
+	
+	public function __construct(&$router) {
+		if (!is_dir($GLOBALS['APP_DIR'].$this->path.$router->params['controller'])){
+			$router->error404('controller view');
+		}
+		$this->action_file = $GLOBALS['APP_DIR']
+								.$this->path
+								.$router->params['controller']
+								.'/'
+								.$router->params['action']
+								.$this->extension;
+		echo $this->action_file."\n";
+		if (!file_exists($this->action_file)) {
+			$router->error404('action view');
+		}
 	}
-	public function render(&$_v, &$actionFile) {
-		include $actionFile;
-;	}
+	
+	public function render(&$_v) {
+		include $this->action_file;
+	}
 }
 	
 ?>
--- lib/doorz/doorz.php
+++ lib/doorz/doorz.php
@@ -5,46 +5,36 @@
 //5 output
 class Doorz {
 	// protected $router;
-	protected $controller;
-	protected $action;
-	protected $view;
-	protected $viewPath = '/app/views/';
-	protected $viewExtension = '.phtml';
+	// protected $controller;
+	// protected $action;
+	// protected $view;
 
 	
 	public function __construct() {
 		$this->dispatch();
-
 	}
 	
 	public function dispatch() {
 
 
 		$router = new Router();
-		$this->view = new BaseView();
 
 		if (!class_exists($router->controller))
 			$router->error404('controller');
-		// check if file controller folder exists
-		// echo $GLOBALS['APP_DIR'].'/app/views/'.$router->params['controller'];
-		if (!is_dir($GLOBALS['APP_DIR'].$this->viewPath.$router->params['controller']))
-			$router->error404('controller view');
 
 		$db = new Database();
 		$c = $db->get_connect();
 
-		$this->controller = new  $router->controller($c);
+		$controller = new  $router->controller($c);
 	
 		$action = $router->action;
-		if (!method_exists($this->controller, $action))
+		if (!method_exists($controller, $action))
 			$router->error404('action');
-		// check if file action exists
-		$actionFile = $GLOBALS['APP_DIR'].$this->viewPath.$router->params['controller'].'/'.$router->params['action'].$this->viewExtension;
-		if (!file_exists($actionFile))
-			$router->error404('action view');
-		$this->controller->$action();
+
+		$controller->$action();
 		
-		$this->view->render($this->controller->view, $actionFile);
+		$view = new BaseView($router);
+		$view->render($controller->view);
 	}
 }
 ?>
