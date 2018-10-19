4b6fa3c91f380310622be90d09632410e7d95ea8
127 <mamyashev.marat@gmail.com>
2018-10-20 00:51:31 +0300

    began model

--- lib/doorz/base_model.php
+++ lib/doorz/base_model.php
@@ -1,15 +1,47 @@
 <?php
 // TODO singletone
 class BaseModel {
-	protected $db;
+	public static $_db;
+	protected $postfix = 'Model';
+	protected $table;
 	
 	public function __construct() {
-		// $this->init_db();
+		$this->table = strtolower(substr(get_class($this), 0, -(strlen($this->postfix)))).'s';
+		// $This->getAllProperties();
+		// echo $this->table;
+		// echo 'Parent class: ' . get_class() . "\n" . 'Child class: ' . get_class($this);
+		$this->call_init();
 	}
-	//
-	// protected function init_db() {
-	// 	$this->db = new PDO ("{$GLOBALS['DB']['driver']}:host={$GLOBALS['DB']['host']}:3306;dbname={$GLOBALS['DB']['database']}", $GLOBALS['DB']['username'], $GLOBALS['DB']['password']);
-	// }
+	
+    protected function call_init(){
+        if(method_exists($this, 'init')){
+            $this->init();
+        }
+    }
+	
+	protected function set_db(&$db){
+		self::$_db = $db;
+	}
+	
+	protected function get_db(){
+		return self::$_db;
+	}
+	
+	protected function save(){
+
+	}
+	
+	protected function update(){
+
+	}
+	
+	protected function delete(){
+
+	}
+	
+	protected function create(){
+
+	}
 }
 	
 ?>
