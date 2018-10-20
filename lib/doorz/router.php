8011632bcfc1728d71832d85f220b9afc3682ea4
127 <mamyashev.marat@gmail.com>
2018-10-21 00:28:20 +0300

    basic routing override done

--- lib/doorz/router.php
+++ lib/doorz/router.php
@@ -3,15 +3,23 @@ class Router {
 	public $controller;
 	public $action;
 	public $params = [];
-	protected $defaultPathController = 'index';
-	protected $defaultPathAction     = 'index';
+	protected $rootController = 'index';
+	protected $rootAction     = 'index';
 	protected $controllerPostfix	 = 'Controller';
 	protected $actionPostfix		 = 'Action';
 	
 	public function __construct() {
+		$f = $GLOBALS['APP_DIR'].'/config/routing.php';
+		if(file_exists($f)){
+			$routing = require_once $f;
+			if(isset($routing['rootControllerPath']))
+				$this->rootController = $routing['rootControllerPath'];
+			if(isset($routing['rootActionPath']))
+				$this->rootAction = $routing['rootActionPath'];
+		}
 		$url = explode('/', $_SERVER['REQUEST_URI']);
-		$this->params['controller'] = ($url[1]!='') ? $url[1] : $this->defaultPathController;
-		$this->params['action'] = (isset($url[2]) && $url[2]!='') ? $url[2] : $this->defaultPathAction;
+		$this->params['controller'] = ($url[1]!='') ? $url[1] : $this->rootController;
+		$this->params['action'] = (isset($url[2]) && $url[2]!='') ? $url[2] : $this->rootAction;
 		$this->controller = ucfirst($this->params['controller']).$this->controllerPostfix;
 		$this->action = ucfirst($this->params['action']).$this->actionPostfix;
 	}
