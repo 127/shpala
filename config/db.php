ca2ced043941f9a6830426872b7336390d85386f
127 <mamyashev.marat@gmail.com>
2018-10-24 22:59:24 +0300

    opened

--- config/db.php
+++ config/db.php
@@ -3,16 +3,16 @@ return array(
 	'development' => array(
 		'driver' => 'mysql',
 		'host' => 'localhost',
-		'username' => 'hrdmn',
-		'password' => 'qwerty127',
-		'database' => 'dev_door'
+		'username' => 'root',
+		'password' => '',
+		'database' => 'development_shpala'
 	),
 	'production' => array(
 		'driver' => 'mysql',
 		'host' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'prod_door'
+		'database' => 'production_shpala'
 	)
 );
 ?>
