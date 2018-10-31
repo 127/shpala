890d4df1a71184575b7ae13d68fdcd1f1ba6be7e
127 <mamyashev.marat@gmail.com>
2018-11-01 00:01:39 +0200

    a lot of tasks done

--- lib/tasks/db/schema/task_load.php
+++ lib/tasks/db/schema/task_load.php
@@ -1,3 +1,9 @@
 <?php
-	
+$f = $GLOBALS['APP_DIR'].'/config/schema.sql';
+if(file_exists($f)){
+	$sql = file_get_contents($f) or die("Can't open {$f}");
+	$c = $db->get_connect();
+	$stmnt = $c->prepare($sql);
+	$stmnt->execute() or die(print_r($stmnt->errorInfo()));
+}
 ?>
--- lib/tasks/db/task_seed.php
+++ lib/tasks/db/task_seed.php
@@ -1,3 +1,6 @@
 <?php
+// $f = $GLOBALS['APP_DIR'].'conf'
+// $sql = file_get_contents('file.sql');
+// $qr = $db->exec($sql);
 
 ?>
