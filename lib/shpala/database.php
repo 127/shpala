2ac9c9ec8b50c60824ab7cadb34f14026303a3b1
Marat Mamiashev <mmamiashev@metaquotes.net>
2018-11-01 11:51:14 +0200

    minor db class refactor

--- lib/shpala/database.php
+++ lib/shpala/database.php
@@ -1,49 +1,52 @@
 <?php
-// TODO total refactor this class
+// TODO total refactor errors with
+//https://gist.github.com/jonahlyn/1186647
 class Database {
+    // private static $instance = null;
+    private $_dbc;
+	private $_config;
+    private $_error = false;
+	// private $_error = 'Cannot connect to the database: ';
 	
-	public  $_connect;
-	public  $_config;
-	private $_dbname;
-	private $_error = 'Cannot connect to the database: ';
-	
-	public function __construct () {
-		$db = require_once $GLOBALS['APP_DIR'].'/config/db.php';
-		$this->_config = $db[$GLOBALS['APP_ENV']];
-		$this->set_connect($this->_config);
-		return $this;
+	public function __construct ($config=false, $set_default_db=true) {
+		$this->set_connect($config);
+		if($set_default_db==true)
+			$this->set_db();
+		return $this->_dbc;
 	}
 	
-	public function set_connect(&$db, $interface = 'pdo'){
+	public function set_connect($config=false, $interface='pdo'){
+		if($config==false)
+			$config = (require_once($GLOBALS['APP_DIR'].'/config/db.php'))[$GLOBALS['APP_ENV']];
+		$this->_config = $config;
 		if ($interface=='mysqli') {
-			$this->_connect = mysqli_connect(
-					$db['host'].':'.$db['port'], 
-					$db['username'], 
-					$db['password'],
-					$db['database']
-				   ) or die($this->_error . mysqli_error());
+			$this->_dbc = mysqli_connect($config['host'].':'.$conf['port'], $config['username'], $config['password']) or die(mysqli_error());
 		} else {
-			//;dbname={$db['database']}
-			$this->_connect = new PDO ("{$db['driver']}:host={$db['host']}:{$db['port']}", $db['username'], $db['password']);
+			try {
+				$this->_dbc = new PDO ("{$config['driver']}:host={$config['host']}:{$config['port']}", $config['username'], $config['password']);
+				return $this->_dbc;
+			} catch (PDOException $e) {
+            	die($e->getMessage());
+        	}
 		}
 	}
 	
-	public function set_db($db = false){
-		if ($db == false)  
-			$db = $this->_config['database'];
-			
-		$this->_connect->exec("use {$db}");
-		$this->_dbname = $db;
+	public function set_db($dbname=false){
+		if($dbname==false)
+			$dbname = $this->_config['database'];
+		$this->_dbc->exec("use {$dbname}");
 	}
 	
 	public function get_db(){
-		return $this->_dbname;
+		return $this->_dbc->query('select database()')->fetchColumn();
 	}
 	
+	// public function get_config(){
+	// 	return $this->_config;
+	// }
+	
 	public function get_connect (){
-		if($this->_dbname==false)
-			$this->set_db();
-		return $this->_connect;
+		return $this->_dbc;
 	}
 
 }
