e2e11f010775a1e455bebe7d9c2ff8f954c0b0e5
127 <mamyashev.marat@gmail.com>
2018-11-03 18:00:53 +0200

    some structure fix

--- db/structure.sql
+++ db/structure.sql
@@ -1,5 +1,5 @@
 CREATE TABLE schema_migrations (
-  version varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  version varchar(255) NOT NULL,
   UNIQUE KEY `unique_schema_migrations` (`version`)
 ) ENGINE=InnoDB;
 
