f544ab51f85d5405d5cb5f6d51cc296ded57ebe9
Marat Mamiashev <mmamiashev@metaquotes.net>
2019-04-12 16:02:35 +0300

    phtml -> html.php + files

---
+++ app/views/layouts/application.html.php
@@ -0,0 +1,12 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?=@$_v['title']?></title>
+	<link type="text/css" rel="stylesheet" href="/_/css/layout.css" />
+  </head>
+
+  <body>		
+  	 <?=$this->render_action()?>
+  </body>
+</html>
